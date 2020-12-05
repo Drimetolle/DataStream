f6b58c4989952a9930b4f7f54f1acb868f659e48
Dmitry <drimetolle@gmail.com>
2020-12-05 17:51:43 +0500

    add sample

--- DataStream/Form1.Designer.cs
+++ DataStream/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 340);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DataStream/Form1.cs
+++ DataStream/Form1.cs
@@ -1,20 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace DataStream
 {
     public partial class Form1 : Form
     {
+        public ChartValues<ObservableValue> Values { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+
+            cartesianChart1.DataTooltip = null;
+            cartesianChart1.Hoverable = false;
+
+            Values = new ChartValues<ObservableValue>
+            {
+                new ObservableValue(3),
+                new ObservableValue(6),
+                new ObservableValue(7),
+                new ObservableValue(4),
+                new ObservableValue(2)
+            };
+
+            cartesianChart1.LegendLocation = LegendLocation.Right;
+
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Values = Values,
+                StrokeThickness = 4,
+                PointGeometrySize = 0
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var r = new Random();
+            Values.Add(new ObservableValue(r.Next(-20, 20)));
         }
     }
 }
